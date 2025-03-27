a1981aa7ed22624083dec8af43fabd0c50dd803a
PatrickWalter214 <patrick.walter214@gmail.com>
2025-03-27 03:59:03 +0100

    Initialize cards component

---
+++ frontend/src/app/components/cards/cards.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { CardComponent } from "../card/card.component";
+
+@Component({
+  selector: 'app-cards',
+  imports: [CardComponent],
+  templateUrl: './cards.component.html',
+  styleUrl: './cards.component.scss'
+})
+export class CardsComponent {
+
+  @Input() values: string[] = [];
+  @Input() submitValueOfCard = (value: string) => {};
+
+  submitValue(value: string): () => void {
+    return () => this.submitValueOfCard(value);
+  }
+
+}
